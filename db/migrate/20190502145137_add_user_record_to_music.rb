131a156855035c1f487eb7b93e9e5a182bc9f925
gmkseta <gmkseta@gmail.com>
2019-05-03 01:53:09 +0900

    asdas

--- db/migrate/20190502145137_add_user_record_to_music.rb
+++ db/migrate/20190502145137_add_user_record_to_music.rb
@@ -1,6 +1,6 @@
 class AddUserRecordToMusic < ActiveRecord::Migration[5.2]
   def change
-    add_reference :musics, :current_record, foreign_key: true, type: :int
-    add_reference :Questions, :current_record, foreign_key: true, type: :int
+    add_column :musics, :current_record, foreign_key: true, type: :int
+    add_column :Questions, :current_record, foreign_key: true, type: :int
   end
 end
