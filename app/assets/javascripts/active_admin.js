c87370544c41ba05fa5439f7fee3c044e1986910
gmkseta <gmkseta@gmail.com>
2019-05-03 02:43:10 +0900

    sasad

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -2,7 +2,7 @@
 //= require active_admin_flat_skin
 
 jQuery(document).ready(function($) {
-$(".fa-pencil").addClass('fa-pencil-alt')
-$(".fa-pencil-alt").addClass('fa-pencil')
-$(".fa-pencil").addClass('fa-pencil-alt')
-}
+    $(".fa-pencil").addClass('fa-pencil-alt')
+    $(".fa-pencil-alt").addClass('fa-pencil')
+    $(".fa-pencil").addClass('fa-pencil-alt')
+})
