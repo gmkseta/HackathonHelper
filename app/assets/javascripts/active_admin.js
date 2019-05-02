8924e482dad11e5cd4553d077dfe32ea9716a31f
gmkseta <gmkseta@gmail.com>
2019-05-03 02:42:00 +0900

    akldjslasdfladsjflkajdf

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,6 +1,8 @@
 //= require active_admin/base
 //= require active_admin_flat_skin
 
-
+jQuery(document).ready(function($) {
 $(".fa-pencil").addClass('fa-pencil-alt')
 $(".fa-pencil-alt").addClass('fa-pencil')
+$(".fa-pencil").addClass('fa-pencil-alt')
+}
