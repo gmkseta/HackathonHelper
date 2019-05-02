ccbcbdcd4148cb2c9bb527b4a9bae8daebc2e404
gmkseta <gmkseta@gmail.com>
2019-05-03 01:40:00 +0900

    rc?

--- db/migrate/20190502145137_add_user_record_to_music.rb
+++ db/migrate/20190502145137_add_user_record_to_music.rb
@@ -1,6 +1,6 @@
 class AddUserRecordToMusic < ActiveRecord::Migration[5.2]
   def change
-    add_reference :musics, :user_record, foreign_key: true
-    add_reference :Questions, :user_record, foreign_key: true
+    add_reference :musics, :user_record, foreign_key: true, type: :int
+    add_reference :Questions, :user_record, foreign_key: true, type: :int
   end
 end
