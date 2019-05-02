5982d2041ff1ae4f4264b3b3da4ed3d8de21d3aa
gmkseta <gmkseta@gmail.com>
2019-05-03 05:01:07 +0900

    asdadsd

--- db/migrate/20190502145137_add_user_record_to_music.rb
+++ db/migrate/20190502145137_add_user_record_to_music.rb
@@ -1,6 +1,6 @@
 class AddUserRecordToMusic < ActiveRecord::Migration[5.2]
   def change
-    add_column :musics, :current_record, :integer
-    add_column :questions, :current_record, :integer
+    add_column :musics, :current_record_id, :integer
+    add_column :questions, :current_record_id, :integer
   end
 end
