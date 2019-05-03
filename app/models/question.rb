1091b579c33e8f984780f65325d3aa3f07ff7f47
gmkseta <gmkseta@gmail.com>
2019-05-04 04:26:33 +0900

    core ui add

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,12 +1,10 @@
 class Question < ApplicationRecord
     belongs_to :user_record, class_name: :UserRecord, foreign_key: "user_record_id"
     after_create :show_board
-
     def show_board
         team = self.user_record.before_record
         group = team.before_record
         Pusher.trigger('qa-channel', 'qa-event', {group: group.content, team: team.content, question: self.content})
-    end
-    
+    end    
 end
 
