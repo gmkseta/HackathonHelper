b840fd78a70c54df072543a8540502655548398e
seenewmax <donaskwhy@nate.com>
2019-05-04 05:08:01 +0900

    fix pusher

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,11 +1,12 @@
 require 'pusher'
 
 secret_key =  Rails.application.credentials[Rails.env.to_sym][:PUSHER]
-
-Pusher.app_id = secret_key[:PUSHER_APP_ID]
-Pusher.key = secret_key[:PUSHER_KEY]
-Pusher.secret = secret_key[:PUSHER_SECRET]
-Pusher.cluster = secret_key[:PUSHER_CLUSTER]
-Pusher.logger = Rails.logger
-Pusher.encrypted = true
-Pusher.timeout = 300000
+unless secret_key.empty?
+  Pusher.app_id = secret_key[:PUSHER_APP_ID]
+  Pusher.key = secret_key[:PUSHER_KEY]
+  Pusher.secret = secret_key[:PUSHER_SECRET]
+  Pusher.cluster = secret_key[:PUSHER_CLUSTER]
+  Pusher.logger = Rails.logger
+  Pusher.encrypted = true
+  Pusher.timeout = 300000
+end
